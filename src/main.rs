e49ed2bd1027b659f09bfa1f69abfa72635e8d8b
Hatcher Blair <hatcherbb@gmail.com>
2025-09-13 14:28:08 -0500

    testing loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn print1_through10() {
+    for i in 1..11 {
+        println!("{}", i);
+    }
+}
+
+fn sum1_through10() -> i32 {
+    let mut tot = 0;
+    for i in 1..10 {
+        tot += i;
+    }
+    tot
+}
+
 fn main() {
     println!("Hello, world!");
+    print1_through10();
+    println!("{}", sum1_through10());
 }
